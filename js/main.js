5cf6a856fd88e0e11983c41edcb289f8baa70e55
olegkrasilev <olegkrasilev@gmail.com>
2020-08-31 12:30:51 +0300

    added anchor animation

--- js/main.js
+++ js/main.js
@@ -24,6 +24,19 @@ $(function(){
         fade: true
         
     });
+
+    $(".menu, .about").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+
+    });
+    
     
 
     AOS.init();
